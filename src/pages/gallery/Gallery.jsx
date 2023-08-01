a48863b7e2d516093637b7a82327b9e51979a0c7
Tamiz07 <tamizh6946@gmail.com>
2023-08-01 20:17:22 +0530

    fifth commit

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,5 +1,6 @@
 import Header from "../../components/Header";
 import HeaderImage from '../../images/header_bg_3.jpg';
+import {AiTwotoneThunderbolt} from 'react-icons/ai';
 import "./gallery.css";
 
 const Gallery = () => {
@@ -22,8 +23,11 @@ const Gallery = () => {
           {
             images.map((image, index) => {
               return (
-                <article key={index}>
+                <article key={index} className="article">
                   <img src={image} alt="" />
+                  <div className="overlay">
+                    <h4 className="logo-text">FIT<span className="thunder"><AiTwotoneThunderbolt /></span><span>LANCE</span></h4>
+                  </div>
                 </article>
               )
             })
